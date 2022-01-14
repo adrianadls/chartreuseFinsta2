ad85a53600ea79fca26cf8809c17addd8ac38e9f
adrianadls <89851640+adrianadls@users.noreply.github.com>
2022-01-14 08:13:21 -0500

    before office hours work

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import PetNav from "./components/pet-nav";
 import PictureBox from "./components/picture-box";
 import AboutBox from "./components/about-box";
-import Followers from "./components/followers.js";
 
 import "./App.css";
 
@@ -16,37 +15,16 @@ const App = () => {
         <div class="container">
           <div class="row">
             <PictureBox />
+          </div>
+          <div class="row">
             <AboutBox />
           </div>
-          <div class="row"></div>
         </div>
       </div>
     );
   };
- 
-  handleFollowerButton = () => {
-    this.setState((state) => {
-      state.followers += 1;
-      return state;
-    });
-  };
 
-  render() {
-    return(
-    <div className = "App">
-      <div className ="followers"
-
-    < Followers
-            id={0}
-            clickHandler={this.handleFollowerButton}
-
-          />
-    </div>
-< /div>
-
-  )}
-
-
+  return component;
 };
 
 export default App;
